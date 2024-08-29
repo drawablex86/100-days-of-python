24fe787beea837eb4c52092c02191f9a88677898
rahulrajeeeev <rahulrajeev1975@gmail.com>
2024-08-29 09:49:21 +0530

    did some random stuff on aug 27

---
+++ 27Aug.py
@@ -0,0 +1,11 @@
+# experimenting with fstring
+def main():
+    name = "Rahul"
+    print(f"his name is {name}")
+    age = int(input("What is you age?: "))
+    print()
+    print("{name}'s age is {age}".format(name = name,age = age))
+
+
+
+main()

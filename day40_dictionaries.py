3cb13a2fc10ff8d9cfd0f818f7618b9063226426
rahulrajeeeev <rahulrajeev1975@gmail.com>
2024-09-06 14:51:37 +0530

    Day 40 - Dictionaries

---
+++ day40_dictionaries.py
@@ -0,0 +1,14 @@
+# testing it out
+import os, time
+while True:
+    myUser = {"name":"", "age":"", "email id":""}
+    print(myUser)
+    print("Let's try Dictionaries of Python\n**********************\n")
+    myUser["name"]= input("Enter your name: ")
+    myUser["age"] = input("Enter your age: ")
+    myUser["email id"] = input("Enter you email id: ")
+
+
+    print(f"\n Your name is {myUser['name']} and you are {myUser['age']} years old and your email id is {myUser['email id']}.")
+    time.sleep(3)
+    os.system("clear")
